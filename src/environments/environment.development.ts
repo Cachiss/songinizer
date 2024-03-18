42c14442456eccec155cd978a702f2f487831ca1
Marcos Cachis <marcoscachis@gmail.com>
2024-03-18 14:33:01 -0600

    aditional styles

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -9,5 +9,10 @@ export const environment = {
         storageBucket: "librosteban.appspot.com",
         messagingSenderId: "845155039941",
         appId: "1:845155039941:web:ce0a56c4d67b7da800ceac"
-            }
+    },
+    spotifyConfig : {
+        client_id : "ded3de0f6bfe44dd8f538d9f2b4018fc",
+        client_secret : "d408b4bc4e834399afc3224fdb585249",
+    }
+
 };
